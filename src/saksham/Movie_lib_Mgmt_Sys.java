18f902351f171583cce5e5acd9e47c42126208b4
Saksham Srivastava <sakshamsrivastava0103@gmail.com>
2026-02-17 12:06:27 +0530

    Delete src directory

--- src/saksham/Movie_lib_Mgmt_Sys.java
+++
@@ -1,5 +0,0 @@
-package saksham;
-
-public class Movie_lib_Mgmt_Sys {
-
-}
--- src/saksham/welcome.java
+++
@@ -1,9 +0,0 @@
-package saksham;
-
-public class welcome {
-	public static void main(String[] args) {
-		System.out.print("Thanks for welcoming us ma'am");
-
-	}
-
-}
